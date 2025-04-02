c16265e7dea94ec11f35bcfe0d0d901665b448b6
XCrocker13 <161492328+XCrocker13@users.noreply.github.com>
2025-04-02 15:38:03 -0600

    Primitive data Type HW

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -1,20 +1,68 @@
-// DataTypes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name;
+	char initial;
+	short age;
+	bool isAdult;
+	unsigned int zipcode;
+	float wage;
+	short daysWorked;
+	float hoursWorkedPerDay[7];
+
+	const float TAX = 0.1f;
+
+	cout << "Enter First name: ";
+	cin >> name;
+
+	cout << "Enter the initial of your Last name: ";
+	cin >> initial;
+
+	cout << "Enter your age: ";
+	cin >> age;
+
+	isAdult = (age >= 18);
+	if (isAdult) {
+
+	cout << "Enter your Zipcode: ";
+	cin >> zipcode;
+
+	cout << "Enter Your desired hourly wage: ";
+	cin >> wage;
+
+	cout << "Enter the amount of days you worked. (Maximum 7): ";
+	cin >> daysWorked;
+
+
+		float totalHours = 0;
+		for (int i = 0; i < daysWorked; i++)
+		{
+			cout << "Enter the hours you worked for day " << (i + 1)
+				<< ": ";
+			cin >> hoursWorkedPerDay[i];
+
+			totalHours += hoursWorkedPerDay[i];
+		}
+
+		float grossIncome = totalHours * wage;
+		float taxAmount = grossIncome * TAX;
+		float netIncome = grossIncome - taxAmount;
+
+		cout << "\nEmployee Payroll Summary\n";
+		cout << "-------------------------\n";
+		cout << "Name: " << name << " " << initial << ".\n";
+		cout << "Age: " << age << " (Adult: " << (isAdult ? "Yes" : "No") << ")\n";
+		cout << "Zipcode: " << zipcode << "\n";
+		cout << "Hourly Wage: $" << wage << " per hour\n";
+		cout << "Total Hours Worked: " << totalHours << " hours\n";
+		cout << "Gross Income: $" << grossIncome << "\n";
+		cout << "Tax Amount: $" << taxAmount << "\n";
+		cout << "Net Income: $" << netIncome << "\n";
+	}
+	else {
+		cout << "You dont qualify \n";
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
